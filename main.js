07647e345ac1d4d95de0c2dbb1e32a6fd8b9f627
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2023-09-18 23:19:45 +0700

    init

---
+++ main.js
@@ -0,0 +1,94 @@
+import puppeteer from "puppeteer";
+
+function sleep(duration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+}
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: process.env.CI === "true",
+  });
+  const page = await browser.newPage();
+
+  await page.goto("https://spada.upnyk.ac.id/login/index.php");
+  await page.waitForNetworkIdle();
+
+  await page.type("#username", process.env.SPADA_USERNAME);
+  await page.type("#password", process.env.SPADA_PASSWORD);
+
+  const loginSelector = "#loginbtn";
+  await page.waitForSelector(loginSelector);
+  await page.click(loginSelector);
+
+  await page.waitForNetworkIdle();
+
+  const courseLinkPrefix = "https://spada.upnyk.ac.id/course/view.php?id=";
+  const courseLinkList = (
+    await page.$$eval("a.list-group-item.list-group-item-action", (elList) =>
+      elList.map((el) => el.href)
+    )
+  ).filter((link) => link.startsWith(courseLinkPrefix));
+
+  const attendanceLinkSelector =
+    "a[href^='https://spada.upnyk.ac.id/mod/attendance/view.php?id=']";
+
+  for await (const courseLink of courseLinkList) {
+    await page.goto(courseLink);
+    await page.waitForNetworkIdle();
+    const courseName = await page.$eval("h1", (el) => el.innerText);
+    console.log(`Mencoba presensi ${courseName}`);
+
+    if (!(await page.$(attendanceLinkSelector))) {
+      console.log("Tidak ada link presensi");
+      continue;
+    }
+
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click(attendanceLinkSelector),
+    ]);
+    await page.waitForNetworkIdle();
+    console.log(
+      await page.$$eval("a", (elList) =>
+        elList
+          .filter((el) => el.innerText.toLowerCase().includes("submit"))
+          .map((el) => [el.href, el.innerText, el.className, el.id])
+      )
+    );
+
+    const submitLinkList = await page.$$eval("a", (elList) =>
+      elList
+        .filter((el) => el.innerText.toLowerCase().includes("submit"))
+        .map((el) => el.href)
+    );
+    if (submitLinkList.length == 0) console.log("Tidak ada link submit");
+    for await (const submitLink of submitLinkList) {
+      console.log(`Mencoba ${submitLink}`);
+      await page.goto(submitLink);
+      await page.waitForNetworkIdle();
+
+      console.log(
+        await page.$$eval("input", (elList) =>
+          elList.map((el) => [el.value, el.className, el.id])
+        )
+      );
+
+      console.log(
+        await page.$$eval("button", (elList) =>
+          elList.map((el) => [el.value, el.className, el.id])
+        )
+      );
+
+      console.log(
+        await page.$$eval("a", (elList) =>
+          elList.map((el) => [el.href, el.innerText, el.className, el.id])
+        )
+      );
+    }
+    await sleep(2000);
+  }
+  await sleep(5000);
+  await browser.close();
+})();
